d7e1ccc4a396f267684360633833a9d5551c8a7e
MUHAMMAD-MUNEEB-WASEEM <50763652+MUHAMMAD-MUNEEB-WASEEM@users.noreply.github.com>
2019-06-09 16:05:50 +0500

    sum initial

--- main.cpp
+++ main.cpp
@@ -4,25 +4,11 @@ using namespace std;
 
 int main()
 {
-    double operand1,operand2;
-    char op;
-    cout<<"Enter desired expression with spaces:";
-    cin>>operand1>>op>>operand2;
-    switch(op)
-    {
-    case '+':
-        cout<<operand1<< "+" <<operand2<<" = "<<operand1+operand2;
-        break;
-    case '-':
-        cout<<operand1<< "-" <<operand2<<" = "<<operand1-operand2;
-        break;
-    case '*':
-        cout<<operand1<< "*" <<operand2<<" = "<<operand1*operand2;
-        break;
-    case '/':
-        cout<<operand1<< "/" <<operand2<<" = "<<operand1/operand2;
-        break;
-    default:
-        cout<<"Invalid operator";
-    }
+    int num1,num2,sum;
+    cout<<"Enter first number:"<<endl;
+    cin>>num1;
+    cout<<"Enter second number:"<<endl;
+    cin>>num2;
+    sum=num1+num2;
+    cout<<"The sum of "<<num1<<" and "<<num2<<" is "<<sum;
 }
